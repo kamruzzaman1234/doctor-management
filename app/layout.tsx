6de9e09746cc57177c2543b067c47c42e00b3b21
kamruzzaman <dmkkaarr.labib980@gmail.com>
2025-11-11 13:56:10 +0600

    First code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from '../components/Shared/Navbar/Nabar'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar></Navbar>
         {children}
+        <h2 className="text-7xl font-bold">Footer Section</h2>
       </body>
     </html>
   );
---
+++ components/Pages/Home/Hero_section/Hero_section.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Hero_Section  =()=>{
+    return(
+        <div className="pt-[120px] pb-[80px] bg-blue-200">
+            <div className="max-w-7xl mx-auto w-full">
+                <div className="">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Hero_Section;
